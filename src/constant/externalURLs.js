5a20d509771904f5d67c7855c407adf8d679ab5a
chai <chai.teng.cn@gmail.com>
2021-06-29 20:46:13 -0400

    test vercel environment

--- src/constant/externalURLs.js
+++ src/constant/externalURLs.js
@@ -1,6 +1,6 @@
 export const DK_LINK = 'https://www.dk-education.com/book-online'
 
-export const LINK_TRACK = 'https://ai.smartmatch.app/member/hytz.php' //跟踪访问信息入口
+export const LINK_TRACK = 'https://ai.smartmatch.app/member/hytz.php'  //跟踪访问信息入口
 
 export const FACEBOOK = 'https://www.facebook.com/DK-105342934694333'
 export const TWITTER = 'https://twitter.com/DK48655550'
