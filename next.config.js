c46ba9b24bcb7f21d59076a175698b007516b6da
chai <chai.teng.cn@gmail.com>
2021-06-29 21:13:49 -0400

    env test

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+      ENV_FLAG: process.env.ENV_FLAG,
+    },
+}
--- src/constant/externalURLs.js
+++ src/constant/externalURLs.js
@@ -8,5 +8,8 @@ export const INSTAGRAM = 'https://www.instagram.com/dk_ca_dk/'
 export const LINKEDIN = 'https://www.linkedin.com/company/dkedu/'
 export const METISIGN_WEBSITE = 'https://www.metisign.com/find-job'
 
-export const APP_END_POINT = process.env.NODE_ENV === 'production' ?  "https://production-api.metisign.com/koios/v1/market_value/" : "https://api.metisign.com/koios/v1/market_value/"
-export const X_API_KEY = process.env.NODE_ENV === 'production' ? 'A?D(G+KbPeShVmYq3t6w9y$B&E)H@McQfTjWnZr4u7x!A%C*F-JaNdRgUkXp2s5v' : '9G3dp8le_wmc8An6ay5lj1J5Hu9baha8em3tvzppcgasiwmc8An6ay5lKa861'
+// export const APP_END_POINT = process.env.NODE_ENV === 'production' ?  "https://production-api.metisign.com/koios/v1/market_value/" : "https://api.metisign.com/koios/v1/market_value/"
+// export const X_API_KEY = process.env.NODE_ENV === 'production' ? 'A?D(G+KbPeShVmYq3t6w9y$B&E)H@McQfTjWnZr4u7x!A%C*F-JaNdRgUkXp2s5v' : '9G3dp8le_wmc8An6ay5lj1J5Hu9baha8em3tvzppcgasiwmc8An6ay5lKa861'
+export const APP_END_POINT = process.env.ENV_FLAG === 'production' ?  "https://production-api.metisign.com/koios/v1/market_value/" : "https://api.metisign.com/koios/v1/market_value/"
+export const X_API_KEY = process.env.ENV_FLAG === 'production' ? 'A?D(G+KbPeShVmYq3t6w9y$B&E)H@McQfTjWnZr4u7x!A%C*F-JaNdRgUkXp2s5v' : '9G3dp8le_wmc8An6ay5lj1J5Hu9baha8em3tvzppcgasiwmc8An6ay5lKa861'
+console.log('envflag = ', process.env.ENV_FLAG )
