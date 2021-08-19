811049dd642df3dffde8cd9827049ff1f999475e
chai <chai.teng.cn@gmail.com>
2021-08-19 00:17:29 -0400

    add endpoint

--- src/pages/api/requestApi.js
+++ src/pages/api/requestApi.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export const applicationHistory = (config) => {
-  const config = {
-    method: 'get',
-    url: 'https://ai.smartmatch.app/chen/job.php?action=shuju&page=1&limit=30'}
-  return axios(config)
+  // const config = {
+  //   method: 'get',
+  //   url: 'https://ai.smartmatch.app/chen/job.php?action=shuju&page=1&limit=30'}
+  // return axios(config)
 }
