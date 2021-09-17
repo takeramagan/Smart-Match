7b24efe1ba2ad48197c539dd6f647a6ea9965024
ygao <764714864@qq.com>
2021-09-17 18:18:47 -0400

    reformate code
    simple reformate code

--- src/pages/report.js
+++ src/pages/report.js
@@ -296,7 +296,7 @@ const SocialMedia = ({ onTrack }) => {
 }
 
 const mock = { market_value_result: [{ matched_job_title: 'Data Scientist', hard_skills_needed_to_improve: ['java', 'project management', 'azure', 'spark', 'r', 'c/c++', 'tableau', 'data engineering'], hard_skill_competitiveness: 88, contract: { market_high: 150, market_low: 42, market_avg: 132, market_mid_low: 85, market_mid_high: 142, predicted_market_value: { high: 135, low: 128 } }, fulltime: { market_high: 170000, market_low: 60000, market_avg: 149600, market_mid_low: 100000, market_mid_high: 160000, predicted_market_value: { high: 150000, low: 140000 } }, projected_career_path: [{ title: 'Senior Data Scientist', market_avg_salary: { fulltime: 153000, contract: 135 }, type: 'Technical' }] }, { matched_job_title: 'Software Engineer', hard_skills_needed_to_improve: ['angular', 'java', 'nosql', 'matlab', 'azure', 'aws', 'c/c++', '.net', 'c++', 'linux', 'react', 'kubernetes', 'c#', 'scala', 'javascript'], hard_skill_competitiveness: 81, contract: { market_high: 100, market_low: 30, market_avg: 81, market_mid_low: 54, market_mid_high: 91, predicted_market_value: { high: 83, low: 78 } }, fulltime: { market_high: 175000, market_low: 56000, market_avg: 141750, market_mid_low: 93000, market_mid_high: 157500, predicted_market_value: { high: 140000, low: 130000 } }, projected_career_path: [{ title: 'Senior Software Engineer', market_avg_salary: { fulltime: 157000, contract: 90 }, type: 'Technical' }] }, { matched_job_title: 'Data Engineer', hard_skills_needed_to_improve: ['data engineering', 'spark', 'aws'], hard_skill_competitiveness: 94, contract: { market_high: 150, market_low: 40, market_avg: 141, market_mid_low: 88, market_mid_high: 147, predicted_market_value: { high: 145, low: 136 } }, fulltime: { market_high: 150000, market_low: 58000, market_avg: 141000, market_mid_low: 94000, market_mid_high: 145000, predicted_market_value: { high: 140000, low: 130000 } }, projected_career_path: [{ title: 'Senior Data Engineer', market_avg_salary: { fulltime: 135000, contract: 135 }, type: 'Technical' }] }], recommended_jobs: [{ job_title: 'Major Projects Data Scientist', job_link: 'https://ca.indeed.com/rc/clk?jk=24159a27f99d1fc4&fccid=f1582c464db8553b&vjs=3', full_time: { salary_range: 'Not Disclosed' }, matched_percentage: 93 }, { job_title: 'Software Engineer Intern (Machine Learning Platform Team - Spring/Summer 2021)', job_link: 'https://ca.indeed.com/rc/clk?jk=ab19da37a974a116&fccid=b8ce556031512ca3&vjs=3', full_time: { salary_range: 'Not Disclosed' }, matched_percentage: 92 }, { job_title: 'Intermediate Machine Learning Engineer', job_link: 'https://ca.indeed.com/rc/clk?jk=14941f7276e243c4&fccid=4512634d9e7338a8&vjs=3', full_time: { salary_range: 'Not Disclosed' }, matched_percentage: 90 }, { job_title: 'Senior Data Scientist/Manager', job_link: 'https://ca.indeed.com/rc/clk?jk=f3689c6e47361dfd&fccid=799362a2faa3b40a&vjs=3', full_time: { salary_range: 'Not Disclosed' }, matched_percentage: 88 }, { job_title: 'Data Architect, Technology Solutions', job_link: 'https://ca.indeed.com/rc/clk?jk=994396db9dbf862b&fccid=b8ee7f714bcca05b&vjs=3', full_time: { salary_range: 'Not Disclosed' }, matched_percentage: 88 }], overall_competitiveness: 8, overall_job_level: 'Senior', experiences_competitiveness: 100, education_competitiveness: 100, soft_skill_competitiveness: 33, soft_skills_needed_to_improve: ['creative', 'communication', 'problem solving', 'time management', 'resourceful', 'adaptive'], hard_skill_competitiveness: 88, hard_skills_needed_to_improve: ['java', 'project management', 'azure', 'spark', 'r', 'c/c++', 'tableau', 'data engineering'], education_levels_needed_to_improve: [] }
-export default function Home () {
+export default function Home() {
 
   const { t } = useTranslation()
   const [viewHistory, setViewHistory] = useState(false)
@@ -325,41 +325,41 @@ export default function Home () {
   const [report, setReport] = useState(null)
 
   // const countryCode = params.countrycode ?? 'ca'
-// console.log('reportid', report_id)
-console.log(params.hrid,params.jobid, index )
-const {requestHandler} = useRequest()
-const getReportFromParams = async() => {
-  setLoading(true)
-  try{
-    console.log('get')
-    const data = new FormData()
-    data.append('hrid', userId); //mock data
-    data.append('jobid', jobid);
-    data.append('dcc', X_API_KEY_B_AND_C);
+  // console.log('reportid', report_id)
+  console.log(params.hrid, params.jobid, index)
+  const { requestHandler } = useRequest()
+  const getReportFromParams = async () => {
+    setLoading(true)
+    try {
+      console.log('get')
+      const data = new FormData()
+      data.append('hrid', userId); //mock data
+      data.append('jobid', jobid);
+      data.append('dcc', X_API_KEY_B_AND_C);
 
-    const config = {
-      method: 'post',
-      url: APP_END_POINT_B_AND_C + ('get_all_applications'),
-      data : data
+      const config = {
+        method: 'post',
+        url: APP_END_POINT_B_AND_C + ('get_all_applications'),
+        data: data
+      }
+      const result = await requestHandler(config)
+      console.log("applicant", result.applicants_info_list)
+      if (!result.status) { //这里返回值 没有status code... T_T
+        // if(result.status === 'success') {
+        console.log("get applicants succcess")
+        console.log(result.applicants_info_list)
+        const applicant = result.applicants_info_list.sort((a, b) => (b.matching_level - a.matching_level))[index]
+        setReport({ ...applicant.report, id: userId, lang })
+      } else {
+        console.log("get applicants error");
+      }
     }
-    const result = await requestHandler(config)
-    console.log("applicant", result.applicants_info_list)
-    if(!result.status) { //这里返回值 没有status code... T_T
-      // if(result.status === 'success') {
-      console.log("get applicants succcess")
-      console.log(result.applicants_info_list)
-      const applicant = result.applicants_info_list.sort((a, b)=> (b.matching_level - a.matching_level))[index]
-      setReport({...applicant.report, id:userId, lang})
-    }else{
-      console.log("get applicants error");
+    catch (e) {
+      console.log("error get applicants");
     }
-  }
-  catch (e) {
-    console.log("error get applicants");
-  }
-  setLoading(false);
+    setLoading(false);
 
-}
+  }
   useEffect(() => {
     if (jobid) getReportFromParams()
   }, [jobid])
@@ -388,7 +388,7 @@ const getReportFromParams = async() => {
     return (
       <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' height='80vh'>
         <Box fontSize='64px' fontWeight='600' color='#49648A'>
-        {t('report.loading')}
+          {t('report.loading')}
         </Box>
         <Box width='600px' m={8}>
           <LinearProgress />
@@ -396,7 +396,7 @@ const getReportFromParams = async() => {
       </Box>
     )
   }
-  
+
   if (!report) {
     return (
       <Box textAlign='center'>
