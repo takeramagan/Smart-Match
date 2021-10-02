a5b0a173e7fb8eeae224f158b6b23ca3e1566b98
Liam <urehelonn@gmail.com>
2021-10-02 13:52:05 -0400

    Update jobmanagement.js
    bring back missing params for check applicant

--- src/pages/jobmanagement.js
+++ src/pages/jobmanagement.js
@@ -1206,7 +1206,7 @@ const JobManagement = () => {
                 <AddApplicant job={hrHistoryList[showItem]} onCancel={closeModal} refreshPage={getData} />
             </Modal>
             <Modal open={showCheckApplicantForm} onClose={closeModal}>
-                <CheckApplicant onCancel={closeModal} refreshPage={getData} />
+                <CheckApplicant  country_code={selectedJobCountry} job_description={selectedJobDescription} onCancel={closeModal} refreshPage={getData} />
             </Modal>
         </Container>
     )
